5b26dc63ec8eb2d5e8a66ec909f1d6567ea42796
Czipo Bence <czipobence@gmail.com>
2015-02-23 19:10:19 +0100

    Robotok sajtá id-val rendelkeznek

--- src/model/Robot.java
+++ src/model/Robot.java
@@ -7,10 +7,14 @@ import model.basic.Velocity;
  * Created by bence on 2015.02.23..
  */
 public class Robot extends TrackObjectBase{
+    private static int ID_COUNT = 0;
+    private int _id;
 
     private Velocity _vel;
 
     public Robot (Position pos) {
+        _id = ID_COUNT;
+        ID_COUNT += 1;
         _pos = pos;
         _vel = new Velocity();
     }
