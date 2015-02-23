23ecad4ee09a28f8bd669475bb313a25036b2943
Czipo Bence <czipobence@gmail.com>
2015-02-23 18:25:01 +0100

    Akadaly osztaly ket tagvaltozoja a hatralevo korok szama es a hatralevo belemenesek szama

--- src/model/Obstacle.java
+++ src/model/Obstacle.java
@@ -5,6 +5,16 @@ package model;
  */
 public abstract class Obstacle {
     private  static double OBSTACLE_RADIUS = 0.5;
+    private static int MAXIMUM_HITS = 3;
+    private static int MAXIMUM_ROUNDS = 20;
 
+    private double _life_left_hits;
+    private double _life_left_rounds;
+
+
+    public Obstacle () {
+        _life_left_hits = MAXIMUM_HITS;
+        _life_left_rounds = MAXIMUM_ROUNDS;
+    }
 
 }
