b0683e776f522c9fc8b34b522cd287fd8237ddb8
Czipo Bence <czipobence@gmail.com>
2015-02-23 18:36:43 +0100

    Pickup konstruktor

--- src/model/Pickup.java
+++ src/model/Pickup.java
@@ -1,8 +1,16 @@
 package model;
 
+import model.basic.Position;
+
 /**
  * Created by bence on 2015.02.23..
  */
 public class Pickup {
+    private Obstacle _obs;
+    private Position _pos;
 
+    public Pickup (Position pos) {
+        //random generálódik hogy mi legyen az obstacle
+        _pos = pos;
+    }
 }
