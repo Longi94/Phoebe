38cd1eb9e0fdddf578788905972c618dc04e525e
Czipo Bence <czipobence@gmail.com>
2015-02-23 15:31:50 +0100

    Ekezdtem egy projektet, egy fasza kis robot osztállyal

---
+++ src/model/Robot.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * Created by bence on 2015.02.23..
+ */
+public class Robot {
+
+    private static double ROBOT_RADIUS = 0.5;
+
+    private int _posX;
+    private int _posY;
+
+    public Robot (int x,int y) {
+        _posX = x;
+        _posY = y;
+    }
+}
