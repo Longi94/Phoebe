c23259c3fa110996f0353cc52fd030854889a744
Czipo Bence <czipobence@gmail.com>
2015-02-23 18:44:39 +0100

    Absztraktá lett ősosztály

--- src/model/TrackObjectBase.java
+++ src/model/TrackObjectBase.java
@@ -3,6 +3,6 @@ package model;
 /**
  * Created by bence on 2015.02.23..
  */
-public class TrackObjectBase {
-    protected static double OBJECT_RADIUS = 0.5;
+public abstract class TrackObjectBase {
+    protected static double RADIUS = 0.5;
 }
