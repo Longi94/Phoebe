035dd460e125e0d1dbef6be241b99881542bcd31
Czipo Bence <czipobence@gmail.com>
2015-02-23 18:20:59 +0100

    Akadaly osztaly absztrakt

--- src/model/Obstacle.java
+++ src/model/Obstacle.java
@@ -3,8 +3,8 @@ package model;
 /**
  * Created by bence on 2015.02.23..
  */
-public class Obstacle {
+public abstract class Obstacle {
     private  static double OBSTACLE_RADIUS = 0.5;
 
-    
+
 }
