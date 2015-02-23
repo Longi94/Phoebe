d7ecb4b0a2d5f3f73eefe0a15b815d38b315ef4c
Czipo Bence <czipobence@gmail.com>
2015-02-23 18:20:30 +0100

    Akadaly osztaly felveve

---
+++ src/model/Obstacle.java
@@ -0,0 +1,10 @@
+package model;
+
+/**
+ * Created by bence on 2015.02.23..
+ */
+public class Obstacle {
+    private  static double OBSTACLE_RADIUS = 0.5;
+
+    
+}
--- src/model/Robot.java
+++ src/model/Robot.java
@@ -82,7 +82,7 @@ public class Robot {
     }
 
     public void forfit() {
-        
+
     }
 
 
