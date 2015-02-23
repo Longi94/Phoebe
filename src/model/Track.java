f7be9478de2bc339754e1ae3146c01ea841b8e99
Czipo Bence <czipobence@gmail.com>
2015-02-23 19:08:23 +0100

    turns left hozzáadva

--- src/model/Track.java
+++ src/model/Track.java
@@ -7,14 +7,19 @@ import java.util.List;
  * Created by geri on 2015. 02. 23..
  */
 public class Track {
+    public static int DEFAULT_TURN_NUMBER = 40;
+
     public List<Obstacle> _obstacles;
     public List<Robot> _robots;
     public List<Pickup> _pickups;
+    public int _turns_left;
 
     public Track() {
+        //TBD ez nagyon nem ilyen lesz
         _obstacles = new ArrayList<Obstacle>();
         _robots = new ArrayList<Robot>();
         _pickups = new ArrayList<Pickup>();
+        _turns_left = DEFAULT_TURN_NUMBER;
     }
 
     /*public boolean addObstacle(Obstacle obs) {
