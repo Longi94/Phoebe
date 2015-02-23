3006db625b0ba28d1cc57963a506bbef81e6a678
Czipo Bence <czipobence@gmail.com>
2015-02-23 18:51:44 +0100

    Track osztály konstruktora

--- src/model/Track.java
+++ src/model/Track.java
@@ -1,10 +1,20 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by geri on 2015. 02. 23..
  */
 public class Track {
+    public List<Obstacle> _obstacles;
+    public List<Robot> _robots;
+    public List<Pickup> _pickups;
 
-    
+    public Track() {
+        _obstacles = new ArrayList<Obstacle>();
+        _robots = new ArrayList<Robot>();
+        _pickups = new ArrayList<Pickup>();
+    }
 
 }
