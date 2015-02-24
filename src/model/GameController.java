9a5e1ca5f37e5b263064df509c6ccf35f29e08d6
Czipo Bence <czipobence@gmail.com>
2015-02-24 10:46:56 +0100

    Game Kontroller osztályba átkerült a _turns_left

--- src/model/GameController.java
+++ src/model/GameController.java
@@ -4,6 +4,9 @@ package model;
  * Created by bence on 2015.02.24..
  */
 public class GameController {
+    public static int DEFAULT_TURN_NUMBER = 40;
+
+    public int _turns_left;
 
     private Robot _myRobot; //kezelt robot;
     private Track _track; //kezelt pálya
@@ -11,6 +14,8 @@ public class GameController {
 
     public GameController() {
         //TBD
+        _turns_left = DEFAULT_TURN_NUMBER;
+
     }
 
 }
