7457167b49de82b102caf688b3a7b1b33657906f
Czipo Bence <czipobence@gmail.com>
2015-02-24 11:24:00 +0100

    Robot kapott nevet

--- src/model/Robot.java
+++ src/model/Robot.java
@@ -11,16 +11,18 @@ public class Robot extends TrackObjectBase{
     private static int START_OIL_AMMOUNT = 1;
     private static int START_PUTTY_AMMOUNT = 1;
     private int id;
+    private String name;
 
     private Velocity vel;
     public int oilAmmount; //rendelkezésre álló olajfoltok mennyisége
     public int puttyAmmount; //rendelkezésre álló ragacsfoltok mennyisége
 
 
-    public Robot (Position pos) {
+    public Robot (String name,Position pos) {
         oilAmmount = START_OIL_AMMOUNT;
         puttyAmmount = START_PUTTY_AMMOUNT;
         id = ID_COUNT;
+        this.name = name;
         ID_COUNT += 1;
         pos = pos;
         vel = new Velocity();
