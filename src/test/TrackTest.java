d7ed42019ee36f5f3be6d1f872261855c9b6c777
Czipo Bence <czipobence@gmail.com>
2015-03-03 14:34:28 +0100

    overLine static lett

--- src/test/TrackTest.java
+++ src/test/TrackTest.java
@@ -1,5 +1,8 @@
 package test;
 
+import org.junit.Assert;
+import model.Track;
+import model.basic.Position;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,6 +11,11 @@ public class TrackTest {
 
     @Test
     public void testOverLine() throws Exception {
-
+        Position p1 = new Position (5,5);
+        Position p2 = new Position (0,0);
+        Position p3 = new Position (10,1);
+        Position p4 = new Position (9,0);
+        Position p5 = new Position (10,10);
+        Assert.assertEquals(1, Track.overLine(p2,p3,p1) * Track.overLine(p4,p5,p1));
     }
 }
