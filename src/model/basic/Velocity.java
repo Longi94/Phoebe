bfaa66e7f8b6ba46c438ac6d83ef7710e40fabbc
Czipo Bence <czipobence@gmail.com>
2015-03-03 12:49:30 +0100

    Velocity is kész :) Ultimate helper ábrával kiegészítve

--- src/model/basic/Velocity.java
+++ src/model/basic/Velocity.java
@@ -7,25 +7,120 @@ package model.basic;
 public class Velocity {
 
     /**
+     * ULTIMATE HELPER ÁBRA
+     *        x <- a bezárt szög
+     *        |
+     *       ||
+     *       |v /
+     *       | /
+     * ______|/_______
+     *       |
+     *       |
+     *       |
+     *    y = r * cos (x)
+     *    x = r * sin(x)
+     *
+     */
+
+    /**
      * A sebességének nagysága
      */
     private double magnitude;
     /**
-     * A sebességének iránya
+     * A sebesség iránya.A szöget a függőleges tengelytől (y tengely) óramutató járásával megegyezően számítjuk.
      */
     private double angle;
 
-    //ide kéne vagy getter setter meg lehet hogy két velocity összeadása is
-
-    public void add(Velocity v) {
-
-    }
+    //=============================================================================================
+    // KONSTRUKTOROK
+    //=============================================================================================
 
     /**
-     * Konstruktor
+     * Paraméter nélküli konstruktor
      */
+
     public Velocity() {
         this.angle = 0;
         this.magnitude = 0;
     }
+
+    //=============================================================================================
+    // GETTER-SETTER-EK
+    //=============================================================================================
+
+    /**
+     * Sebesség suggara polárkotdinátás számítás esetén
+     * @return a sugár
+     */
+    public double getMagnitude() {
+        return magnitude;
+    }
+
+    /**
+     * Beállítja a sebesség polárkoordinátás sugarát
+     * @param magnitude a sugár
+     */
+    public void setMagnitude(double magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    /**
+     * Polárkoordinátás számítás esetén a sebesség szögét adja meg
+     * @return a szöge
+     */
+    public double getAngle() {
+        return angle;
+    }
+
+    /**
+     * Polárkoordinátás számítás esetén a sebesség szögét állítja be
+     * @param angle a szög
+     */
+    public void setAngle(double angle) {
+        this.angle = angle;
+    }
+
+
+    /**
+     * A sebességet adja vissza descartes-i koordináta rendszerben.
+     * @return a sebességvektor x koordinátája
+     */
+    public double getVelocityX () {
+        return magnitude * Math.sin(angle);
+    }
+
+    /**
+     * A sebességet adja vissza descartes-i koordináta rendszerben.
+     * @return a sebességvektor y koordinátája
+     */
+    public double getVelocityY() {
+        return magnitude * Math.cos(angle);
+    }
+
+    /**
+     * Descares-i koordináták alapján beállítja a sebességet
+     * @param x x koordinátája a sebességvektornak
+     * @param y y koordinátája a sebességvektornak
+     */
+    public void setDescartes(double x, double y) {
+        this.magnitude = Math.sqrt(x*x+y*y);
+        //Ha az első félsíkban van, az acos  helyes eredményt ad, viszont ha a második félsíkban van, akkor
+        //cos(x) = cos(2PI-x) miatt nem a megfelelő eredményt kapjuk
+        if (x>0) {
+            this.angle = Math.acos(y/this.magnitude);
+        } else {
+            this.angle = 2*Math.PI - Math.acos(y/this.magnitude);
+        }
+    }
+
+    /**
+     * Hozzáad a sebességhez egy másikat
+     * @param v a másik, hozzáadandó sebesség
+     */
+    public void add(Velocity v) {
+        double tempX = v.getVelocityX() + this.getVelocityX();
+        double tempY = v.getVelocityY() + this.getVelocityY();
+        this.setDescartes(tempX, tempY);
+    }
+
 }
