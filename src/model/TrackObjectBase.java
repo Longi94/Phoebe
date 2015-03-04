f5072e9a75ab73f5e05063d1314aaaae6be01fd7
Long Tran <lngtrn94@gmail.com>
2015-03-04 19:49:02 +0100

    attemp conflict kezelésre

--- src/model/TrackObjectBase.java
+++ src/model/TrackObjectBase.java
@@ -10,23 +10,46 @@ public abstract class TrackObjectBase {
     protected Position pos;
     protected Track track;
 
+    /**
+     * Konstruktor
+     * @param pos az objektum pozíciója
+     * @param track a pálya, amin az objektum található
+     */
+    public TrackObjectBase(Position pos, Track track) {
+        this.pos = pos;
+        this.track = track;
+    }
+
+    /**
+     * Getter a sugárhoz
+     * @return a sugár
+     */
     public static double getRadius() {
         return RADIUS;
     }
 
-    //TODO rosszul van paraméterezve szekvencia diagrammon
+    /**
+     * Megvizsgálja, ütközik-e az adott objektummal az objektum
+     * @param otherObject az objektum amivel vizsgáljuk hogy ütközik-e
+     * @return true, ha ütköznek és false ha nem
+     */
     public boolean hit(TrackObjectBase otherObject) {
         double distance = Math.sqrt(Math.pow(this.pos.getX() - otherObject.pos.getX(), 2) + Math.pow(this.pos.getY() - otherObject.pos.getY(), 2));
 
-        return distance <= RADIUS + otherObject.getRadius(); //Szerintem semmi baj a 2*RADIUS-szal
+        return distance <= RADIUS + otherObject.getRadius();
     }
 
-
+    /**
+     * Akkor hívódik meg,ha ütközik az objektummal egy robot
+     * @param r a robot, amivel ütközik
+     */
     public void collide (Robot r)  {
 
     }
 
-    
+    /**
+     * Új kör esetén meghívódó függvény
+     */
     public void newRound() {}
 
     public Position getPos() {
