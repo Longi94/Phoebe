6f963da6c18d9a386c6d1ace34169a84a01ae55f
Czipo Bence <czipobence@gmail.com>
2015-03-18 19:22:22 +0100

    ezt most csak azért kommitolom hogy pullolhassak :)

--- src/skeleton/usecases/RobotJumpIntoOil.java
+++ src/skeleton/usecases/RobotJumpIntoOil.java
@@ -4,6 +4,7 @@ import model.Oil;
 import model.Robot;
 import model.Track;
 import model.basic.Position;
+import model.basic.Velocity;
 
 import java.util.ArrayList;
 
@@ -19,8 +20,13 @@ public class RobotJumpIntoOil {
 
         Track t = new Track(in,out);
 
-        t.addObject(new Robot(new Position(0,0), t, "Robot1"));
+        Robot r = new Robot(new Position(0,0), t, "Robot1");
+
+        t.addObject(r);
         t.addObject(new Oil(new Position(1,1),t));
+
+        r.jump(new Velocity());
+
     }
 
 }
