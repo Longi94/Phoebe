3c376b3f680a0d8d4fd3404d5ba636602dbf99b7
Long Tran <lngtrn94@gmail.com>
2015-03-18 17:45:56 +0100

    message fv (nem tesztelt)

--- src/Log.java
+++ src/Log.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ThanhLong on 2015.03.16..
  */
 public class Log {
+
+    private static List<Integer> indexes = new ArrayList<Integer>();
+    private static boolean returned = false;
 
     /**
      * Függvény hívás
@@ -12,6 +18,37 @@ public class Log {
      */
     public static void message(String objectName, String methodName, String... parameters) {
 
+        String log = "";
+
+        if (!returned) {
+            indexes.add(1);
+        } else {
+            int temp = indexes.get(indexes.size() - 1);
+            indexes.set(indexes.size() - 1, ++temp);
+        }
+
+        for (int i = 1; i < indexes.size(); i++) {
+            log += "----";
+        }
+
+        log += ">";
+
+        for (int i = 0; i < indexes.size() - 1; i++) {
+            log += "" + indexes.get(i) + ".";
+        }
+        log += indexes.get(indexes.size() - 1) + ":";
+
+        log += objectName + "." + methodName + "(";
+
+        if (parameters.length > 0) {
+            for (int i = 0; i < parameters.length - 1; i++) {
+                log += parameters[i] + ", ";
+            }
+        }
+        log += parameters[parameters.length - 1] + ")";
+
+        returned = false;
+        System.out.println(log);
     }
 
     /**
@@ -19,6 +56,7 @@ public class Log {
      */
     public static void returnMessage() {
         System.out.println("<----");
+        returned = true;
     }
 
     /**
@@ -28,5 +66,6 @@ public class Log {
      */
     public static void returnMessage(String returnParameter) {
         System.out.println("<----" + returnParameter);
+        returned = true;
     }
 }
