a72ca0b1247995a0057bf747295fc787eb72c085
anonymus4096 <anonymus4096@gmail.com>
2015-03-19 18:17:48 +0100

    usecase 5

---
+++ src/skeleton/usecases/RobotJumpOutOfTrack.java
@@ -0,0 +1,44 @@
+package skeleton.usecases;
+
+import model.Robot;
+import model.Track;
+import model.basic.Position;
+import model.basic.Velocity;
+import skeleton.PhoebeLogger;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Benedek on 2015.03.19..
+ */
+public class RobotJumpOutOfTrack {
+    public RobotJumpOutOfTrack(){
+        PhoebeLogger.enableLogging(false);
+        PhoebeLogger.clear();
+
+        ArrayList<Position> in = new ArrayList<Position>();
+        ArrayList<Position> out = new ArrayList<Position>();
+
+        out.add(new Position(-1, -1));
+        out.add(new Position(11, -1));
+        out.add(new Position(11, 11));
+        out.add(new Position(-1, 11));
+
+        in.add(new Position(1,1));
+        in.add(new Position(9, 1));
+        in.add(new Position(9,9));
+        in.add(new Position(1,9));
+
+        //DUMMY NÉGYZET ALAKÚ PÁLYA
+
+        Track t = new Track(in,out);
+
+        Robot r = new Robot(new Position(0,0), t, "R2D2");
+
+        t.addObject(r);
+
+        r.jump(new Velocity(90, 1));
+
+    }
+
+}
