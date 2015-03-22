c2640f9af617b84c1bbbb935220bbffb7c031cd5
Long Tran <lngtrn94@gmail.com>
2015-03-22 21:58:23 +0100

    kikommentelt logger

--- src/skeleton/PhoebeLogger.java
+++ src/skeleton/PhoebeLogger.java
@@ -8,8 +8,13 @@ import java.util.List;
  */
 public class PhoebeLogger {
 
+    //Engedélyezve van-e a loggolás
     private static boolean enabled = false;
+
+    //Ebben tároljuk el a sorszámozást
     private static List<Integer> indexes = new ArrayList<Integer>();
+
+    //Figyelni kell hogy éppen visszatértünk-e a függvényből vagy nem
     private static boolean returned = false;
 
     /**
@@ -20,30 +25,39 @@ public class PhoebeLogger {
      * @param parameters metódusnak átadott paraméterek
      */
     public static void message(String objectName, String methodName, String... parameters) {
+        //Nem loggolunk, ha le van tiltva
         if (!enabled) return;
 
+        //Ezzel kezdődik mindig
         String log = "----";
 
         if (!returned) {
+            //Eggyel mélyebbre mentünk
             indexes.add(1);
         } else {
+            //Maradunk ezen a szinten, inkrementálunk
             int temp = indexes.get(indexes.size() - 1);
             indexes.set(indexes.size() - 1, ++temp);
         }
 
+        //Nyíl hossza a mélységtől függ
         for (int i = 1; i < indexes.size(); i++) {
             log += "----";
         }
 
+        //Nyílvég
         log += ">";
 
+        //Hozzácsapjuk a sorszámozást
         for (int i = 0; i < indexes.size() - 1; i++) {
             log += "" + indexes.get(i) + ".";
         }
         log += indexes.get(indexes.size() - 1) + ":";
 
+        //Hozzácsapjuk a meghívott objektum és függvény nevét
         log += objectName + "." + methodName + "(";
 
+        //Ha megadtak paramétereket, akkor azt is hozzácsapjuk a Stringhez majd lezárjuk zárójellel
         if (parameters.length > 0) {
             for (int i = 0; i < parameters.length - 1; i++) {
                 log += parameters[i] + ", ";
@@ -53,7 +67,10 @@ public class PhoebeLogger {
             log += ")";
         }
 
+        //Kiírjuk a logot
         System.out.println(log);
+
+        //Nyílván
         returned = false;
     }
 
@@ -70,22 +87,32 @@ public class PhoebeLogger {
      * @param returnParameter a visszaadott paraméter
      */
     public static void returnMessage(String returnParameter) {
+        //Nem loggolunk, ha le van tiltva
         if (!enabled) return;
 
-        if (indexes.size() == 0)
+        //Dobjunk kivételt, ha nincs hova vissza menni
+        if (indexes.size() == 0 || (indexes.size() == 1 && returned))
             throw new IllegalStateException();
 
+        //Kiinduló string
         String log = "";
+
+        //Egy indexet törlünk, ha egy szintel lejjeb léptünk
         if (returned) {
             indexes.remove(indexes.size() - 1);
         }
+
+        //Minél mélyebben vagyunk, annál beljebb van a nyíl
         for (int i = 0; i < indexes.size() - 1; i++) {
             log += "    ";
         }
-        log += "<----";
-        System.out.println(log + returnParameter);
-        returned = true;
 
+        //Maga a nyíl
+        log += "<----";
+        //Visszatérő érték hozzácsapása
+        System.out.println(log + returnParameter);
+
+        returned = true;
     }
 
     /**
@@ -112,29 +139,39 @@ public class PhoebeLogger {
      * @param variableName változó neve
      */
     public static void create(String className, String variableName) {
+        //Nem loggolunk, ha le van tiltva
+        if (!enabled) return;
 
         if (!returned) {
+            //Eggyel mélyebbre mentünk
             indexes.add(1);
         } else {
+            //Maradunk ezen a szinten, inkrementálunk
             int temp = indexes.get(indexes.size() - 1);
             indexes.set(indexes.size() - 1, ++temp);
         }
 
         returned = false;
 
+        //Ezzel kezdődik mindig
         String log = "----";
 
+        //Nyíl hossza a mélységtől függ
         for (int i = 1; i < indexes.size(); i++) {
             log += "----";
         }
 
+        //Nyílvég
         log += ">";
 
+        //Hozzácsapjuk a sorszámozást
         for (int i = 0; i < indexes.size() - 1; i++) {
             log += "" + indexes.get(i) + ".";
         }
+        //A create tag
         log += indexes.get(indexes.size() - 1) + ":<<create>>";
 
+        //A változó neve és típusa
         log += variableName + ":" + className;
 
         System.out.println(log);
