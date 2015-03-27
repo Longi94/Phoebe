53398c56591b593ab9cae580cd261afb259d5da6
Czipo Bence <czipobence@gmail.com>
2015-03-27 21:52:45 +0100

    CleaningRobot osztály hozzáadva

---
+++ src/model/CleaningRobot.java
@@ -0,0 +1,14 @@
+package model;
+
+import model.basic.Position;
+
+/**
+ * Created by bence on 2015.03.27..
+ */
+public class CleaningRobot extends TrackObjectBase {
+
+    public CleaningRobot(Position pos) {
+        super(pos);
+    }
+
+}
