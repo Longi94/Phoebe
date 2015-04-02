9cb8ddd62cba9781855a5f4f4c5ea802762fc5f7
Long Tran <lngtrn94@gmail.com>
2015-04-02 19:44:13 +0200

    be és kimenetek inicializálása

--- src/Phoebe.java
+++ src/Phoebe.java
@@ -1,9 +1,42 @@
+import java.io.*;
+
 /**
  * Created by ThanhLong on 2015.03.04..
  */
 public class Phoebe {
-
+    /**
+     * A játék belépő (epszilon ha..ha..) main függvénye
+     * @param args program argumentumok
+     */
     public static void main(String[] args) {
+        //Be és kimenetek inicalizálása
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        try {
+            switch (args.length) {
+                case 0:
+                    //Nincs paraméter, konzol a ki és bemenet
+                    br = new BufferedReader(new InputStreamReader(System.in));
+                    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+                    break;
+                case 1:
+                    //1 paraméter, konzol a kimenet, fájl a bemenet
+                    br = new BufferedReader(new FileReader(args[0]));
+                    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+                    break;
+                default:
+                    //2 vagy több paraméter, fájl a ki és bemenet
+                    br = new BufferedReader(new FileReader(args[0]));
+                    bw = new BufferedWriter(new FileWriter(args[1]));
+            }
+        } catch (FileNotFoundException e) {
+            //TODO nem talált fájl kezelése
+            e.printStackTrace();
+        } catch (IOException e) {
+            //TODO do something...
+            e.printStackTrace();
+        }
+
     }
 
 }
