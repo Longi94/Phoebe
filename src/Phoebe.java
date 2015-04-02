489a5cf155973dea5f03f4428419412d53a291fe
Long Tran <lngtrn94@gmail.com>
2015-04-02 19:16:48 +0200

    újra kezdjük a maint

--- src/Phoebe.java
+++ src/Phoebe.java
@@ -1,60 +1,9 @@
-import model.GameController;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 /**
  * Created by ThanhLong on 2015.03.04..
  */
 public class Phoebe {
-    // ===========================================================
-    // Constants
-    // ===========================================================
-
-    // ===========================================================
-    // Fields
-    // ===========================================================
-
-    // ===========================================================
-    // Constructors
-    // ===========================================================
-
-    // ===========================================================
-    // Getter & Setter
-    // ===========================================================
-
-    // ===========================================================
-    // Methods for/from SuperClass/Interfaces
-    // ===========================================================
-
-    // ===========================================================
-    // Methods
-    // ===========================================================
 
     public static void main(String[] args) {
-        //Játékosok számának bekérése
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int playerCount = 0;
-        while (playerCount == 0){
-            System.out.print("Jatekosok szama: ");
-            try{
-                playerCount = Integer.parseInt(br.readLine());
-            } catch (IOException e) {
-                System.out.println("Valami nagyon nem jo ha ez kiirodik");
-            } catch (NumberFormatException e) {
-                System.out.println("Rossz formatum");
-            }
-        }
-
-        //controller inicalizálása és elindítása
-        GameController controller = new GameController();
-        controller.setNumberOfPlayers(playerCount);
-        controller.initGame();
-
     }
 
-    // ===========================================================
-    // Inner and Anonymous Classes
-    // ===========================================================
 }
