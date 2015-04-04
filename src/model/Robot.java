c866657162fb02d0ff25435c12b6dec0bc7879c8
Czipo Bence <czipobence@gmail.com>
2015-04-04 17:32:04 +0200

    A robot-robot ütközést javítottam

--- src/model/Robot.java
+++ src/model/Robot.java
@@ -159,14 +159,16 @@ public class Robot extends TrackObjectBase {
     @Override
     public void collide(Robot r) {
         if (vel.getMagnitude() >= r.vel.getMagnitude()) {
-            //vel.add(r.getVel());
-            //halveVelocity();
-            setVel(new Velocity((vel.getMagnitude() + r.vel.getMagnitude()) / 2, (vel.getAngle() + r.vel.getAngle()) / 2));
+            Velocity tmp = this.getVel();
+            tmp.add(r.getVel());
+            tmp.setMagnitude(tmp.getMagnitude()/2);
+            this.setVel(tmp);
             track.removeObject(r);
         } else {
-            //r.getVel().add(this.vel);
-            //r.halveVelocity();
-            r.setVel(new Velocity((vel.getMagnitude() + r.vel.getMagnitude()) / 2, (vel.getAngle() + r.vel.getAngle()) / 2));
+            Velocity tmp = r.getVel();
+            tmp.add(this.vel);
+            tmp.setMagnitude(tmp.getMagnitude()/2);
+            r.setVel(tmp);
             track.removeObject(this);   //egyenloseg eseten marad, akire ralepnek (pl jatek elejen jol johet)
         }
         // igazából a többi objectel ütközést megállíthatnánk, de fölösleges, mert elvileg úgyis minden egyidőben zajlik le (és az, hogy kivettük a többől, nem okoz durva problémát)
