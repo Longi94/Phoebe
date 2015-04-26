cdc481d6c46a5ed5ce5f118c2283024199d207b1
Long Tran <lngtrn94@gmail.com>
2015-04-26 12:56:22 +0200

    hiányzó field

--- src/view/PickupView.java
+++ src/view/PickupView.java
@@ -1,5 +1,7 @@
 package view;
 
+import model.Pickup;
+
 import java.awt.*;
 
 /**
@@ -9,6 +11,8 @@ import java.awt.*;
  * @since 2015.04.25.
  */
 public class PickupView extends TrackObjectBaseView{
+
+    private Pickup pickup;
 
     public void draw(Graphics graph, double xOffset, double yOffset, double zoom) {
 
