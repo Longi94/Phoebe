e12cd9c53424b84af9c6bb88934b8faf532200ea
anonymus4096 <anonymus4096@gmail.com>
2015-04-28 11:42:59 +0200

    háttér beállítása + komponensek

--- src/view/MenuView.java
+++ src/view/MenuView.java
@@ -1,7 +1,12 @@
 package view;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Menü panel
@@ -27,15 +32,33 @@ public class MenuView extends JPanel {
     private JPanel startPanel;
 
     /**
+     * A körök számát kiválasztó menürész
+     */
+    private JPanel roundsPanel;
+
+    private BufferedImage background;
+
+    /**
      * Kiválasztott pálya neve (kiterjesztéssel együtt)
      */
     String selectedMap;
 
     public MenuView() {
+        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
+        // háttérkép beolvasása
+        try {
+            background = ImageIO.read(new File("assets/img/menu_background_new.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // Pálya választó megjelenítése
         initSelectTrackPanel();
         add(selectTrackPanel);
+
+        initRoundsPanel();
+        add(roundsPanel);
 
         // Játékosok panel megjelenítése
         initPlayersPanel();
@@ -46,9 +69,18 @@ public class MenuView extends JPanel {
         add(startPanel);
     }
 
+    private void initRoundsPanel() {
+        roundsPanel = new JPanel();
+        roundsPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        roundsPanel.setOpaque(false);
+
+    }
+
     public void initSelectTrackPanel() {
 
         selectTrackPanel = new JPanel();
+        selectTrackPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        selectTrackPanel.setOpaque(false);
 
         String[] trackListArray = GameController.getAvailableTracks();
 
@@ -60,32 +92,38 @@ public class MenuView extends JPanel {
 
         JComboBox<String> trackList= new JComboBox<String>(trackListArray);
 
+        JLabel trackLabel = new JLabel("Select track:");
+        selectTrackPanel.add(trackLabel);
         selectTrackPanel.add(trackList);
+
 
     }
 
     public void initPlayersPanel() {
 
         playersPanel = new JPanel();
-
+        playersPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        playersPanel.setOpaque(false);
 
 
     }
 
     public void initStartPanel() {
 
-        startPanel = new JPanel(new BorderLayout());
+        startPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        startPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        startPanel.setOpaque(false);
 
         JButton startButton = new JButton("Start");
-        startPanel.add(startButton,BorderLayout.CENTER);
+        startPanel.add(startButton);
 
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+
         // Háttérkép rajzolása
-        Image background = Toolkit.getDefaultToolkit().createImage("assets/img/menu_background.jpg");
         g.drawImage(background, 0, 0, null);
     }
 
